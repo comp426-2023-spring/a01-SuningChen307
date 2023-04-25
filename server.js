00b5c6b243e5a2f78589234767bc56973cf32128
SuningChen <tuning@live.unc.edu>
2023-04-25 13:56:19 -0400

    fix1

--- server.js
+++ server.js
@@ -3,12 +3,11 @@ const http = require('http');
 // Require fs module
 const fs = require('fs');
 // Require minimist module (make sure you install this one via npm).
-const minimist = require('minimist');
+const argv = require('minimist')(process.argv.slice(2));
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-let args = minimist(process.agrs.slice(2));
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = args.port || 3000;
+const port = argv['port'] || 3000;
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
@@ -18,8 +17,6 @@ fs.readFile('./public/index.html',(err, data) => {
           console.error(err);
           return;
     }
-    
-});
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
 
@@ -30,13 +27,12 @@ fs.readFile('./public/index.html',(err, data) => {
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
    const server = http.createServer((req, res) => {
-         res.statuscode = 200;
-         res.setHeader('Content-Type', 'text/html');
-         res.end('./public/index.html');
-   })
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		res.end(data);
+});
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
-	server.listen(port);
-        console.log("Server listening on port " + port);
+   server.listen(port, console.log('Server listening on port', port));
+});
 
 // That's it! You're all done!
